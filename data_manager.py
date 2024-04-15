21f75cb056c2c2e9e40f7edb31a4bd5348a331da
Chq Sun <52816600+qi657@users.noreply.github.com>
2024-04-15 10:25:08 +0800

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -15,7 +15,7 @@ class TrainDataset(data.Dataset):
         self.config = config
 
         train_list_file = os.path.join(config.datasets_dir, config.train_list)
-        # 如果数据集尚未分割，则进行训练集和测试集的分割
+
         if not os.path.exists(train_list_file) or os.path.getsize(train_list_file) == 0:
             files = os.listdir(os.path.join(config.datasets_dir, 'ground_truth'))
             random.shuffle(files)
